afffc16026427456d02ddfab8a35bc7a21bfa107
Abdel Mejia <28445496+Bhalut@users.noreply.github.com>
2024-07-09 17:33:59 -0500

    refactor: Update application configuration and routing
    - Added `provideAnimationsAsync` to the application configuration for async animation support.
    - Updated routes to include lazy loading for Star Wars and File Management modules.
    - Set default route to redirect to the Star Wars module.
    - Added wildcard route to handle unknown routes by redirecting to the Star Wars module.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'star-wars',
+    loadChildren: () =>
+      import('./star-wars/star-wars.module').then((m) => m.StarWarsModule),
+  },
+  {
+    path: 'file-management',
+    loadChildren: () =>
+      import('./file-management/file-management.module').then(
+        (m) => m.FileManagementModule
+      ),
+  },
+  { path: '', redirectTo: '/star-wars', pathMatch: 'full' },
+  { path: '**', redirectTo: '/star-wars' },
+];
