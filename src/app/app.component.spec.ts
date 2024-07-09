e0aebf2ba2ec63a23c34f7c60620bbce40dc3873
Abdel Mejia <28445496+Bhalut@users.noreply.github.com>
2024-07-09 17:33:30 -0500

    refactor: Update AppComponent to include navigation and routing
    - Added navigation menu with router links for Movies, Characters, Starships, Vehicles, Species, and File Upload components.
    - Updated styles for navigation and active links.
    - Set up routing configuration for Star Wars components and File Upload component.
    - Refactored AppComponent to use RouterLink for navigation.
    - Updated AppComponent tests to include routing setup and verify the presence of navbar links.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,59 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 
+import { MoviesComponent } from './star-wars/movies/movies.component';
+import { CharactersComponent } from './star-wars/characters/characters.component';
+import { SpeciesComponent } from './star-wars/species/species.component';
+import { StarshipsComponent } from './star-wars/starships/starships.component';
+import { VehiclesComponent } from './star-wars/vehicles/vehicles.component';
+
+import { FileUploadComponent } from './file-management/file-upload/file-upload.component';
+
+const routes: Routes = [
+  { path: 'movies', component: MoviesComponent },
+  { path: 'characters', component: CharactersComponent },
+  { path: 'species', component: SpeciesComponent },
+  { path: 'starships', component: StarshipsComponent },
+  { path: 'vehicles', component: VehiclesComponent },
+  { path: 'file-upload', component: FileUploadComponent },
+];
+
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterModule.forRoot(routes),
+        AppComponent,
+        MoviesComponent,
+        CharactersComponent,
+        SpeciesComponent,
+        StarshipsComponent,
+        VehiclesComponent,
+        FileUploadComponent,
+      ],
     }).compileComponents();
-  });
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'starfile' title`, () => {
+  it(`should have as title 'StarFile'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('starfile');
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('StarFile');
   });
 
-  it('should render title', () => {
+  it('should render title in a navbar link', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, starfile');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('nav ul li a').textContent).toContain(
+      'Movies'
+    );
   });
 });
